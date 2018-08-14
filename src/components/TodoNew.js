a0047c98de1e504641342b6a826a9be52d7d0c89
Gustavo Fernandes dos Santos <gsantos@bridge-mt.com>
2018-08-14 13:23:40 -0300

    14 ago

--- src/components/TodoNew.js
+++ src/components/TodoNew.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
 
 export default props => (
-    <form 
-        className={css(styles.form)}
+    <form
         onSubmit={props.handleTextSubmit}>
-        <input 
-            className={css(styles.inputText)}
-            type="text" value={props.texto} onChange={props.handleTextChange} />
-        <input 
-            className={css(styles.inputButton)} 
-            type="submit" value="Adicionar" />
+        <div className="row">
+            <input
+                className={css(styles.inputText)}
+                type="text" value={props.texto} onChange={props.handleTextChange} />
+        </div>
+        <div className="row">
+            <input
+                className={css(styles.inputButton)}
+                type="submit" value="Adicionar" />
+        </div>
     </form>
 )
 
@@ -18,25 +21,30 @@ const styles = StyleSheet.create({
     form: {
         display: 'inline-block',
         flex: '0 0 100%',
-        background: '#005792',
+        background: '#FFF',
         borderRadius: '0.2rem',
         boxShadow: '0px 4px 29px -5px rgba(0,0,0,0.75)',
-        maxWidth: '24rem',
+        maxWidth: '32rem',
         width: '100%',
         marginDown: '1rem',
         marginTop: '1rem',
         padding: '1rem'
     },
     inputText: {
-        background: '#f6f6e9',
-        color: '#13334c',
+        background: '#F0F0F0',
+        color: '#1A1A1A',
+        width: '90%',
         padding: '1rem',
-        borderStyle: 'none'
+        borderStyle: 'none',
+        borderRadius: '0.5rem'
     },
     inputButton: {
-        background: '#fd5f00',
-        color: '#f6f6e9',
+        background: '#1A1A1A',
+        color: '#F0F0F0',
+        boxShadow: '0px 4px 29px -5px rgba(0,0,0,0.75)',
         padding: '1rem',
-        borderStyle: 'none'
+        marginLeft: '0.5rem',
+        borderStyle: 'none',
+        borderRadius: '0.5rem'
     }
 })
